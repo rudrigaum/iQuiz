bbb4150d0f53442d858aa20a200778d37e1a7e3f
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2023-05-02 07:08:14 -0300

    Adding IBOutlet, IBAction and setup Corner Radius in Button

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var buttonQuizStart: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureLayout()
     }
 
-
+    @IBAction func pressedButton(_ sender: Any) {
+    
+    }
+    
+    func configureLayout() {
+        buttonQuizStart.layer.cornerRadius = 12.00
+    }
+    
 }
 
