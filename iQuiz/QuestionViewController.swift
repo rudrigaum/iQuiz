fb673d2995275e92f9a504222208ade8c9caacbc
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2023-05-03 08:24:18 -0300

    Adding Buttons on Quiz Screen

---
+++ iQuiz/QuestionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionViewController.swift
+//  iQuiz
+//
+//  Created by Rodrigo Cerqueira Reis on 02/05/23.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true 
+    }
+    
+
+}
